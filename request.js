e3d3e12ee3c717da986be37fd4f61bb4d1c0cd39
alfiya <faizralf@gmail.com>
2021-01-31 22:29:47 +0300

    4

--- request.js
+++ request.js
@@ -7,40 +7,13 @@ https({
         'Content-Type': 'application/json',
     },
     data: {
-        name: 'Ivan',
+        name: 'Vlad',
         password: '456',
     },
 })
-.then(res => {
-  if (res.data) {
-    https({
-      method: 'post',
-      url: 'http://localhost:8080/accesses',
-      headers: {
-          'Content-Type': 'application/json',
-          'token': res.data,
-      },
-    })
-    .then(response => {
-      console.log(response.data);
-    });
-  }
+.then(response => {
+  console.log(response.data);
 })
 .catch(err => {
     console.log('error >>>> ', err)
-});
-
-https({
-  method: 'post',
-    url: 'http://localhost:8080/accesses',
-    headers: {
-        'Content-Type': 'application/json',
-        'token': 'token456',
-    }
-})
-.then(res => {
-  console.log(res.data);
-})
-.catch(err => {
-  console.log('error >>>> ', err)
 });
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const dbInfo = 'info.json';
-let token = '';
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 
@@ -20,52 +18,32 @@ app.post('/', (request, response, next) => {
     response.end('You need to authenticate');
 });
 
+mongoose.connect('mongodb://localhost:27017/mongodb');
+const UserSchema = mongoose.Schema({name: String, password: String});
+const User = mongoose.model('Users', UserSchema);
+
 app.post('/tokens', (request, response, next) => {
-    token = '';
     if (request.body.name && request.body.password) {
-        console.log('I will ask the DB does it know you');
-        if (fs.existsSync(dbInfo)) {
-            const info = JSON.parse(fs.readFileSync(dbInfo, 'utf8'));
-            const userGood = (info) => {
-                for (let i = 0; i < info.length; i++) {
-                    if (
-                        info[i].name === request.body.name &&
-                        info[i].password === request.body.password
-                    ) {
-                        token = info[i].token;
-                        return true;
-                    }
-                }
-                return false
-            };
-            if (userGood(info)) {
-                console.log('I\'ve sent you the token');
-                response.end(token);
-            } else {
-                console.log('The DB doesn\'t know you');
-                response.end(false);
+        console.log('I will add your data to the DB');
+        const user = new User({name: request.body.name, password: request.body.password});
+        user.save((error, savedUser) => {
+            if (error) {
+                throw error;
             }
-        } else {
-            console.log('The DB doesn\'t exist');
-            response.end(false);
-        }
+            response.send(`Hi there ${savedUser.name}, I know your password: ${savedUser.password}`)
+        });
     } else {
         console.log('I\'ve not gotten any data');
         response.end('You need to send user\'s data');
     }
 });
 
-app.post('/accesses', (request, response, next) => {
-    if (request.headers.token === token) {
-        console.log('I can show you the content');
-        response.end('Congratulations! You have access to content');
-    } else if (request.headers.token) {
-        console.log('Your token is wrong');
-        response.end('Try again');
-    } else {
-        console.log('I\'ve not gotten the token');
-        response.end('You need to send token');
-    }
+app.listen(8080, () => {
+    console.log('Server listening at post at port 8080');
+    User.find({}, (err, user) => {
+        console.log(
+            'In the collection at the moment: ',
+            user.map(u => u.name).join(' ')
+        );
+    })
 });
-
-app.listen(8080, console.log('Server listening at post at port 8080'));
